2028792f67a61428a77d03b79408428daf1a9a5f
SieteNred <sietenred@gmail.com>
2021-02-26 11:54:35 +0100

    configurando rutas

--- secondApp/src/app/app-routing.module.ts
+++ secondApp/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
 
-const routes: Routes = [];
+// componentes
+import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
+
+
+const routes: Routes = [
+  { path: '', component: ListarProductosComponent },
+  { path: 'crear-producto', component: CrearProductoComponent},
+  { path: 'editar-producto/:id', component: CrearProductoComponent},
+  { path: '**', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
