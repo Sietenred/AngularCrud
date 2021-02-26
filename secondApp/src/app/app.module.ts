d2577cb9d3dbbf68d1251468cc5c930858635647
SieteNred <sietenred@gmail.com>
2021-02-26 11:06:41 +0100

    añadiendo componentes

--- secondApp/src/app/app.module.ts
+++ secondApp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
+import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrearProductoComponent,
+    ListarProductosComponent
   ],
   imports: [
     BrowserModule,
